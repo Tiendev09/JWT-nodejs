9cbb3dc3ba6808dc4744de2857c00dbb93ff9ba8
Tiendev09 <manhtiendev1997@gmail.com>
2022-05-05 00:59:43 +0700

    second commit

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,10 @@
 import express from "express";
-
+import homeController from "../controller/homeController";
 const router = express.Router();
 //req:request,  res: response
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world") //phan hoi
-    })
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/about", homeController.handleUser);
     return app.use("/", router);
 }
 export default initWebRoutes;
